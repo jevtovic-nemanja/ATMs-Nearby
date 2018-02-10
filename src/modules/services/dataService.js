7c047b60bbcad71ebf5fb0d2e1c4ef5aae9d94c2
nemanja-jevtovic <jevta993@gmail.com>
2018-02-10 23:28:25 +0100

    add function for fetching closest atms data

--- src/modules/services/dataService.js
+++ src/modules/services/dataService.js
@@ -8,6 +8,31 @@ class DataService {
         const userCoordinates = new UserCoordinates(latitude, longitude);
         return userCoordinates;
     }
+
+    getAtmData(userCoordinates, successCallback, errorCallback) {
+        const mapOptions = {
+            center: {
+                lat: userCoordinates.lat,
+                lng: userCoordinates.lng
+            },
+            zoom: 15
+        };
+
+        const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        const request = {
+            location: mapOptions.center,
+            radius: "50000",
+            type: ["atm"]
+        };
+
+        const service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, (results, status) => {
+            status === "OK"
+                ? successCallback(results)
+                : errorCallback(status);
+        });
+    }
 }
 
 export const dataService = new DataService();
