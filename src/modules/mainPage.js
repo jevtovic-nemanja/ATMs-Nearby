6b96f197828f215c32ff10778bbee770a65bfc98
nemanja-jevtovic <jevta993@gmail.com>
2018-02-10 19:00:20 +0100

    display interface on load

--- src/modules/mainPage.js
+++ src/modules/mainPage.js
@@ -1,7 +1,21 @@
+import { appendChildren } from "../utils/helpers";
+
 import { getUserLocation } from "./services/geolocationService";
 
 const app = document.querySelector(".app");
 
+const displayGoButton = () => {
+    const div = document.createElement("div");
+    const message = document.createElement("span");
+    const goButton = document.createElement("button");
+
+    message.textContent = "Find nearby ATMs";
+    goButton.textContent = "Go";
+
+    appendChildren(div, message, goButton);
+    appendChildren(app, div);
+};
+
 export const onPageLoad = () => {
-    app.textContent = "The app is running.";
+    displayGoButton();
 };
