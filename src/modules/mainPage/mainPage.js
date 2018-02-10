c1a3f61c955b7fcb226c6c41eea4289a982d79f6
nemanja-jevtovic <jevta993@gmail.com>
2018-02-10 19:23:21 +0100

    add error container div

--- src/modules/mainPage/mainPage.js
+++ src/modules/mainPage/mainPage.js
@@ -8,12 +8,13 @@ const displayGoButton = () => {
     const div = document.createElement("div");
     const message = document.createElement("span");
     const goButton = document.createElement("button");
+    const errorContainer = document.createElement("div");
 
     message.textContent = "Find nearby ATMs";
     goButton.textContent = "Go";
     goButton.disabled = true;
 
-    appendChildren(div, message, goButton);
+    appendChildren(div, message, goButton, errorContainer);
     appendChildren(app, div);
 };
 
