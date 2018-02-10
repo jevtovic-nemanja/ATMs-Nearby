e700eeaf040b0f3830088de89098b5a508520b8c
nemanja-jevtovic <jevta993@gmail.com>
2018-02-10 20:14:57 +0100

    fix position object property name

--- src/modules/services/dataService.js
+++ src/modules/services/dataService.js
@@ -1,7 +1,7 @@
 import UserLocation from "../entities/userLocation";
 
 export const getUserLocation = position => {
-    const { latitude, longitude } = position.coordinates;
+    const { latitude, longitude } = position.coords;
     const userLocation = new UserLocation(latitude, longitude);
     return userLocation;
 };
