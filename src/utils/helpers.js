5ea0745c6511d730f6f3b619c11624858b113b23
nemanja-jevtovic <jevta993@gmail.com>
2018-02-11 15:11:14 +0100

    add helper for formatting distance

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,5 @@
 export const appendChildren = (node, ...children) => {
     children.forEach(childNode => node.appendChild(childNode));
 };
+
+export const formatDistance = distance => distance < 1 ? distance * 1000 + " meters" : distance + " km";
